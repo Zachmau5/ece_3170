8689a02b1166b1b5bd73bb640e0c2d0f641bfb25
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2024-01-16 19:35:54 -0700

    Update lab2.asm

--- lab2.asm
+++ lab2.asm
@@ -1,30 +1,33 @@
-$include (C8051F020.inc)
+$include (c8051f020.inc) ; Include the C8051F020 definitions
 
-        ORG 0000h ; origin
+ORG 0000h ; Start of program
 
-        MOV     SP, #7FH ; set stack pointer
+; Initialization
+mov wdtcn, #0DEh ; disable watchdog
+mov wdtcn, #0ADh
+mov xbr2, #40h ; enable port output
 
-        ; Initialize Ports
-        MOV     P1MDOUT, #00h ; Set P1 as input for pushbuttons
-        MOV     P2MDOUT, #00h ; Set P2 as input for DIP switches
-        MOV     P3MDOUT, #0FFh ; Set P3 as output for LEDs
+; Configure ports
+mov P1MDIN, #0xFF ; Set P1 as input
+mov P2MDIN, #0xFF ; Set P2 as input
+mov P3MDOUT, #0xFF ; Set P3 as output
+mov P4MDOUT, #0xFF ; Set P4 as output
 
-        ; Initialize Crossbar and Ports
-        MOV     XBR2, #40h ; Enable Crossbar and weak pull-ups
+MAIN_LOOP:
+; Read state of pushbuttons
+mov A, P1 ; Load pushbutton states into accumulator
+; Invert the bits since a pushbutton press is active low
+cpl A
+; Reflect state on the first two LEDs
+mov P3, A ; Only P3.0 and P3.1 are affected
 
-        ; Disable Watchdog Timer
-        MOV     WDTCN, #0DEh
-        MOV     WDTCN, #0ADh
+; Read state of DIP switches
+mov A, P2 ; Load DIP switch states into accumulator
+; Invert the bits since a DIP switch ON is active low
+cpl A
+; Reflect state on the eight LEDs
+mov P4, A ; Reflect on P4.0 to P4.7
 
-MainLoop:
-        MOV     A, P1 ; Read pushbuttons
-        ANL     A, #03h ; Mask for the first two bits
-        CPL     A ; Invert for active-low LEDs
-        MOV     B, A ; Store the state of pushbuttons
-        MOV     A, P2 ; Read DIP switches
-        MOV     P3, A ; Reflect DIP switches on LEDs
-        ORL     P3, B ; Combine pushbutton state with DIP switches
+SJMP MAIN_LOOP ; Loop back to start
 
-        SJMP    MainLoop ; Repeat indefinitely
-
-        END
+END ; End of program
