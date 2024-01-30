be4d6a21de614b5fbc5ca1cdba15a3790f401b03
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2024-01-29 19:44:33 -0700

    slightly kinda working ish tugowar.asm

--- tugowar.asm
+++ tugowar.asm
@@ -14,7 +14,7 @@ mov wdtcn, #0ADh
 mov xbr2, #40h              ; enable port output
 setb P2.7                   ; Input button (right)
 setb P2.6                   ; Input button (left)
-mov Position, #03h          ; Starting position for P3.2 (LED3)
+mov Position, #05h          ; Starting position for P3.5 (LED3)
 
 ; Initialize and Display LEDs
 LCALL Display
@@ -44,11 +44,11 @@ NONE:
 
 Game_over:
     MOV A, Position
-    CJNE A, #00H, NINE      ; Check if position is at the left extreme
+    CJNE A, #0AH, NINE      ; Check if position is at the left extreme
     SJMP OVER               ; End the game
 NINE:
     MOV A, Position
-    CJNE A, #08H, MainLoop  ; Check if position is at the right extreme
+    CJNE A, #00H, MainLoop  ; Check if position is at the right extreme
     SJMP OVER               ; End the game
 
 OVER:
@@ -77,43 +77,43 @@ Display:
 ; ------------- Display LED's ----------------
 DISP_LED:
     CJNE A, #1, not_led1
-    CLR P3.0                ; Turn on LED1
+    CLR P2.0                ; Turn on LED1
     RET
 not_led1:
     CJNE A, #2, not_led2
-    CLR P3.1                ; Turn on LED2
+    CLR P2.1                ; Turn on LED2
     RET
 not_led2:
     CJNE A, #3, not_led3
-    CLR P3.2                ; Turn on LED3 (initially on)
+    CLR P3.0                ; Turn on LED3 (initially on)
     RET
 not_led3:
     CJNE A, #4, not_led4
-    CLR P3.3                ; Turn on LED4 (initially on)
+    CLR P3.1                ; Turn on LED4 (initially on)
     RET
 not_led4:
     CJNE A, #5, not_led5
-    CLR P3.4                ; Turn on LED5
+    CLR P3.2                ; Turn on LED5
     RET
 not_led5:
     CJNE A, #6, not_led6
-    CLR P3.5                ; Turn on LED6
+    CLR P3.3                ; Turn on LED6
     RET
 not_led6:
     CJNE A, #7, not_led7
-    CLR P3.6                ; Turn on LED7
+    CLR P3.4                ; Turn on LED7
     RET
 not_led7:
     CJNE A, #8, not_led8
-    CLR P3.7                ; Turn on LED8
+    CLR P3.5                ; Turn on LED8
     RET
 not_led8:
     CJNE A, #9, not_led9
-    CLR P2.0                ; Turn on LED9
+    CLR P3.6                ; Turn on LED9
     RET
 not_led9:
     CJNE A, #10, DISP_END
-    CLR P2.1                ; Turn on LED10
+    CLR P3.7                ; Turn on LED10
     RET
 DISP_END:
     RET
