2a2b9a27c8f165fff7f1109dbfe7a166fd07b961
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2024-03-18 18:54:38 -0600

    Update lcd.asm

--- lab_6/lcd.asm
+++ lab_6/lcd.asm
@@ -5,7 +5,7 @@ public init_lcd, refresh_screen, blank_screen, screen, font5x8;
 
 $include (c8051f020.inc)
 LCD_CMD   equ 8000H ; Set this to the address of the command register
-LCD_DAT   equ 9F00H ; Set this to the address of the data register
+LCD_DAT   equ 8000H ; Set this to the address of the data register
 LCD_RESET equ 10H ; Mask that selects the reset line on P4 (e.g. for P4.4 use 10H)
 
 ;
@@ -114,8 +114,9 @@ byte_loop:
 
 blank_screen:
 	mov	dptr,#0
-	mov	a,#0
+	mov	a,#1
 blank_loop:
+	rr A
 	movx	@dptr,a
 	inc	dptr
 	mov	b,dph
