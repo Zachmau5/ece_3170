6ddcdfd94184827ef93228c3de2ff433cd0353a9
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2024-03-26 22:50:27 -0600

    Update lcd.h

--- lab_7/lcd.h
+++ lab_7/lcd.h
@@ -1,4 +1,4 @@
-
+//
 // LCD Interface
 //
 // This module initializes the 64x128 LCD module, declares a shadow memory
@@ -10,28 +10,25 @@
 // It sets up LCD hardware, blanks the shadow memory then displays it on
 // the screen.
 //
-extern xdata unsigned char screen[];
 void init_lcd(void);
-void init_device(void);
-void init_adc(void);
+
 //
 // Copy shadow memory to LCD screen.
 //
 void refresh_screen(void);
-void disp_char(unsigned char row, unsigned char col, char ch);
-extern code char font[];
+
 //
 // Clear the shadow memory.
 //
 void blank_screen(void);
-void lcd_write_string(char *str, unsigned char line, unsigned char row);
+
 //
 // Shadow memory. 1024 bytes. Eight 128-byte pages. Each page corresponds
 // to 8 rows of pixels. screen[0] is upper left, screen[127] is upper right,
 // screen[1023] is lower right. Least significant bit of each byte is on the
 // top pixel row of its page.
 //
-//extern xdata char screen[];
+extern xdata char screen[];
 
 //
 // Handy 5x7 font that will come in handy in later labs. Always put at least
